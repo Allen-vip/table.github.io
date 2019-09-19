fa13d966543477e94ea03be945c491d5ed258a48
zhuyang <zhuyang07172@hellobike.com>
2019-09-19 14:08:12 +0800

    build

---
+++ precache-manifest.3c71a84797574c153de28e00272e1cde.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528bc996a2895007febff5c395d04df8",
+    "url": "/table.github.io/index.html"
+  },
+  {
+    "revision": "4734725ec28d8066a159",
+    "url": "/table.github.io/static/css/2.be02ba6f.chunk.css"
+  },
+  {
+    "revision": "f368d855cec78e60c35d",
+    "url": "/table.github.io/static/css/main.6165b328.chunk.css"
+  },
+  {
+    "revision": "4734725ec28d8066a159",
+    "url": "/table.github.io/static/js/2.9c7edabc.chunk.js"
+  },
+  {
+    "revision": "f368d855cec78e60c35d",
+    "url": "/table.github.io/static/js/main.736068f6.chunk.js"
+  },
+  {
+    "revision": "b2abaf57f48cda921b3f",
+    "url": "/table.github.io/static/js/runtime~main.0a8af0dc.js"
+  },
+  {
+    "revision": "95b9de606a00dd2d2269b56e9b2b6e2f",
+    "url": "/table.github.io/static/media/examples@2x.95b9de60.png"
+  },
+  {
+    "revision": "3a608dcf55d6ad524a843fc20cd95524",
+    "url": "/table.github.io/static/media/img_bg_Congrats@2x.3a608dcf.png"
+  },
+  {
+    "revision": "d571cd025ae59e5d989ae43fb10f69cd",
+    "url": "/table.github.io/static/media/img_giftcard_bg_failure@2x.d571cd02.png"
+  },
+  {
+    "revision": "1e1a29a013125046cb8520c76cee8574",
+    "url": "/table.github.io/static/media/img_giftcard_bg_normal@2x.1e1a29a0.png"
+  },
+  {
+    "revision": "38c2040e1b0374622119f4a6bb2486d7",
+    "url": "/table.github.io/static/media/scratch_card@2x.38c2040e.png"
+  },
+  {
+    "revision": "170cc29e1b503b80b07cc5ad24b41108",
+    "url": "/table.github.io/static/media/successful.170cc29e.png"
+  },
+  {
+    "revision": "79b6e611c93f6ec7e83ff20be09da1fe",
+    "url": "/table.github.io/static/media/sumary_NeedHelp@2x.79b6e611.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25c83ebbbc1966f542b0666d334bc713.js"
+  "/table.github.io/precache-manifest.3c71a84797574c153de28e00272e1cde.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/table.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
